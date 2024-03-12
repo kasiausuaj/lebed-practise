523853d825fc7dbc513bbe5bcda3d6d2a5cb7f6c
kasiausuaj <andrejlebed120@gmail.com>
2024-03-12 19:23:37 +0700

    починил баг с открытием сайта

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,20 +2,19 @@
 //Путь до директории с конфигурационными файлами
 const DIR_CONFIG = '/../config';
 
-
 //Добавляем пользовательскую функцию автозагрузки классов
 spl_autoload_register(function ($className) {
-   $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-   $className = str_replace('\\', '/', $className);
-
-   foreach ($paths['classes'] as $path) {
-       $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
-       if (file_exists($fileName)) {
-           require_once $fileName;
-       }
-   }
-});
-
+    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+    $className = str_replace('\\', '/', $className);
+ 
+    foreach ($paths['classes'] as $path) {
+        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$paths[root]/$path/$className.php";
+        if (file_exists($fileName)) {
+            require_once $fileName;
+        }
+    }
+ });
+ 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
@@ -28,5 +27,7 @@ function getConfigs(string $path = DIR_CONFIG): array
    }
    return $settings;
 }
+
+require_once __DIR__ . '/../routes/web.php';
 
 return new Src\Application(new Src\Settings(getConfigs()));
