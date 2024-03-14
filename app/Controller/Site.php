5db5f078462234d2ee7fd31a5faacf83052f52c9
kasiausuaj <andrejlebed120@gmail.com>
2024-03-14 22:59:36 +0700

    создание регистрации и логина

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -19,12 +19,13 @@ class Site
    {
        return new View('site.hello', ['message' => 'hello working']);
    }
-   public function signup(Request $request): string
-   {
-       if ($request->method==='POST' && User::create($request->all())){
-           return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
-       }
-       return new View('site.signup');
-   }
+    public function signup(Request $request): string
+    {
+    if ($request->method === 'POST' && User::create($request->all())) {
+        app()->route->redirect('/go');
+    }
+    return new View('site.signup');
+    }
+
 
 }
