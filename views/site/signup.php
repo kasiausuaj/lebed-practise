10d7954f5bdc52fc343a17cb14da0dbd1211bb60
kasiausuaj <andrejlebed120@gmail.com>
2024-03-20 21:02:33 +0700

    базовая защита/валидация

--- views/site/signup.php
+++ views/site/signup.php
@@ -1,6 +1,8 @@
 <h2>Регистрация нового пользователя</h2>
 <h3><?= $message ?? ''; ?></h3>
 <form method="post">
+<input name="csrf_token" type="hidden" value="<?= app()->auth::generateCSRF() ?>"/>
+
    <label><input type="text" name="surname"placeholder="Фамилия"></label>
    <label><input type="text" name="name"placeholder="Имя"></label>
    <label><input type="text" name="patronym" placeholder="Отчество"></label>
